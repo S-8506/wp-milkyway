356b3db7ea3b6d2d3fb41a3e04272e58aa6a3ec8
S-8506 <s.8506.a@gmail.com>
2021-08-02 17:16:04 +0530

    inject stylesheet

--- functions.php
+++ functions.php
@@ -4,4 +4,15 @@
  * @package milkyway
  */
 
+
+ function milkyway_enqueue_default_scripts(){
+         wp_enqueue_style('stylesheet',get_stylesheet_uri(),[],filemtime(get_template_directory(). '/style.css'),'all');
+       
+       
+         /*  wp_enqueue_style('main-css',get_template_directory_uri(). '/main.css',['stylesheet']);*/
+ }
+
+ add_action('wp_enqueue_scripts','milkyway_enqueue_default_scripts');
+
+
 ?>
